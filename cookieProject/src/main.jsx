2ab58472b5b819eeb3fa740ff33e3c3735e03b0d
SteijeDeBroek <steije.debrucker@student.hogent.be>
2023-10-22 18:45:04 +0200

    Upload.jsx basics
    de basis van de upload-page, mogelijk al 1 bestand te uploaden

--- cookieProject/src/main.jsx
+++ cookieProject/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/jsx-no-undef */
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Upload from "./assets/Upload.jsx";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {/* <App /> */}
+    <Upload />
+  </React.StrictMode>
+);
