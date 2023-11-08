dd1b64bb2d2f96f3458ee2bb25fe144b3015c6a8
IlyaQuateau <ilya.quateau@student.hogent.be>
2023-11-08 16:19:39 +0100

    Auto generating header added

--- cookieProject/src/components/HeaderComponent.jsx
+++ cookieProject/src/components/HeaderComponent.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 
+import { pages } from "../routes/Pages";
+import { Link } from "react-router-dom";
+
 const HeaderComponent = () => {
+  const generateLink = (page) => {
+    if (page.headerLabel != null) {
+      return (
+        <li key={page.path}>
+          <Link to={page.path}>{page.headerLabel}</Link>
+        </li>
+      );
+    }
+    return null;
+  };
+
   return (
     <header>
-      <h1 className="text-bold">Hello!</h1>
+      <nav aria-label="Global">
+        <ul className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+          {pages.map((page) => {
+            return generateLink(page);
+          })}
+        </ul>
+      </nav>
     </header>
   );
 };
--- cookieProject/src/routes/Pages.jsx
+++ cookieProject/src/routes/Pages.jsx
@@ -9,31 +9,37 @@ import NotFoundPage from "../pages/NotFoundPage";
 
 export const pages = [
   {
+    headerLabel: null,
     path: "/",
     element: <Navigate replace to="/home" />,
     shouldLogin: false,
   },
   {
+    headerLabel: "Home",
     path: "/home",
     element: <HomePage />,
     shouldLogin: false,
   },
   {
+    headerLabel: "Login",
     path: "/login",
     element: <LoginPage />,
     shouldLogin: false,
   },
   {
+    headerLabel: "Upload",
     path: "/upload",
     element: <UploadPage />,
     shouldLogin: true,
   },
   {
+    headerLabel: "Vote",
     path: "/vote",
     element: <VotePage />,
     shouldLogin: true,
   },
   {
+    headerLabel: null,
     path: "*",
     element: <NotFoundPage />,
     shouldLogin: false,
