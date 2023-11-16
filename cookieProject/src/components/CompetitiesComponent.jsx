b780377ebc645b6aaa499525eebc18b7825fbb2d
SteijeDeBroek <steije.debrucker@student.hogent.be>
2023-11-16 14:12:46 +0100

    DIV cleanup
    moet wel nog variable programmeren, alles is hard gecodeerd

--- cookieProject/src/components/CompetitiesComponent.jsx
+++ cookieProject/src/components/CompetitiesComponent.jsx
@@ -4,7 +4,7 @@ import "../css/Competities.css"; /* Hij leest de css file niet? */
 const CompetitiesComponent = () => {
   return (
     <div className="pl-40">
-      <div className="flex items-center justify-between p-10  border border-orange-400 bg-orange-300 rounded">
+      <div className="flex items-center justify-between h-96 p-10  border border-orange-400 bg-orange-300 rounded">
         <div id="winnaars" className="border border-black rounded">
           <div id="winnaars">
             <img
@@ -14,7 +14,7 @@ const CompetitiesComponent = () => {
               alt=""
               className="hover:h-68 hover:w-44 cursor-pointer"
             />
-            <p className="font-sans font-semibold text-clip">
+            <p className="font-sans font-semibold">
               Naam Gerecht 1 met lasagna
             </p>
           </div>
@@ -51,7 +51,7 @@ const CompetitiesComponent = () => {
         </div>
       </div>
 
-      <div className="flex items-center  justify-between p-10 mt-5 border border-green-400 bg-green-200  rounded ">
+      <div className="flex items-center h-96  justify-between p-10 mt-5 border border-green-400 bg-green-200  rounded ">
         <div id="winnaars" className="border border-black rounded">
           <img
             src="https://scontent-bru2-1.xx.fbcdn.net/v/t39.30808-6/260417119_4675057169268479_4379047456459630175_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=5f2048&_nc_ohc=iKU7FLYcby8AX-9x4ip&_nc_ht=scontent-bru2-1.xx&oh=00_AfBuYa6GThfN067epjVJKK7OvIgNdEzhSLVbRQYZ3lTzjQ&oe=6558C9EF"
@@ -93,7 +93,7 @@ const CompetitiesComponent = () => {
           <p>Naam Gerecht</p>
         </div>
       </div>
-      <div className="flex items-center justify-between p-10 mt-5 border border-blue-400 bg-blue-200  rounded ">
+      <div className="flex items-center justify-between h-96 p-10 mt-5 border border-blue-400 bg-blue-200  rounded ">
         <div id="winnaars" className="border border-black rounded">
           <img
             src="https://scontent-bru2-1.xx.fbcdn.net/v/t39.30808-6/260417119_4675057169268479_4379047456459630175_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=5f2048&_nc_ohc=iKU7FLYcby8AX-9x4ip&_nc_ht=scontent-bru2-1.xx&oh=00_AfBuYa6GThfN067epjVJKK7OvIgNdEzhSLVbRQYZ3lTzjQ&oe=6558C9EF"
