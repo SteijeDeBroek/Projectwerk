003a521503106a1e20ffbeb5f7356e67111b7d95
stan599 <stan.paridaens@student.hogent.be>
2023-11-16 14:12:54 +0100

    EersteController
    tryout

--- Backend/Cookiemonster/Program.cs
+++ Backend/Cookiemonster/Program.cs
@@ -8,6 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddSwaggerGen();
+
+
 
 //Create an instance of IConfiguration and load the appsettings.json
 var configuration = new ConfigurationBuilder()
