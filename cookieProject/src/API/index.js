da6cf903b5c4995d243075e9543afe4305b0a891
SteijeDeBroek <steije.debrucker@student.hogent.be>
2023-11-30 13:52:00 +0100

    begin API Axios

---
+++ cookieProject/src/API/index.js
@@ -0,0 +1,69 @@
+import axios from "axios"
+
+//definieer de functies
+//categories
+export const getCategories = async () => {
+return await axios.get("")
+}
+export const deleteCategories = async () => {
+return await axios.get("")
+}
+
+export const postCategories = async () => {
+return await axios.get("")
+}
+//images
+export const getImages = async () => {
+return await axios.get("")
+}
+
+export const deleteImages = async () => {
+return await axios.get("")
+}
+
+export const postImages = async () => {
+return await axios.get("")
+}
+//recipes
+export const getRecipes = async () => {
+return await axios.get("")
+}
+export const deleteRecipes = async () => {
+return await axios.get("")
+}
+export const postRecipes = async () => {
+return await axios.get("")
+}
+//todos
+export const getTodos = async () => {
+return await axios.get("")
+}
+export const deleteTodos = async () => {
+return await axios.get("")
+}
+
+export const postTodos = async () => {
+return await axios.get("")
+}
+//Users
+export const getUsers = async () => {
+return await axios.get("")
+}
+export const deleteUsers = async () => {
+return await axios.get("")
+}
+
+export const postUsers = async () => {
+return await axios.get("")
+}
+//votes
+export const getVotes = async () => {
+return await axios.get("")
+}
+export const deleteVotes = async () => {
+return await axios.get("")
+}
+
+export const postVotes = async () => {
+return await axios.get("")
+}
