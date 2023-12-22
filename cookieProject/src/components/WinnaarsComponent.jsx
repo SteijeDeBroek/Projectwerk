b32ff35ee74a116c39add205a244ff7593c7e043
IlyaQuateau <ilya.quateau@student.hogent.be>
2023-12-22 17:22:57 +0100

    Winnaars styling update

--- cookieProject/src/components/WinnaarsComponent.jsx
+++ cookieProject/src/components/WinnaarsComponent.jsx
@@ -36,20 +36,23 @@ class WinnaarsComponent extends Component {
     }
 
     const imageStyle = {
-      backgroundImage: `url(data:image/jpg;base64,${this.state.image.base64Image})`,
+      backgroundImage: `linear-gradient(rgba(0, 0, 0, 0) 25%, rgba(0, 0, 0, 0.9)), url(data:image/jpg;base64,${this.state.image.base64Image})`,
       backgroundSize: "cover",
       backgroundPosition: "center",
       height: "200px",
       width: "250px",
+      padding: "10px",
       display: "flex",
       flexDirection: "column", // Stack items vertically
       justifyContent: "flex-end", // Align items to the bottom
+      // border: "1px solid transparent",
+      // borderRadius: "25px",
     };
 
     return (
       <div
         id="winnaars"
-        className="border border-black rounded"
+        className="border border-gray-300 rounded-2xl shadow-lg hover:shadow-xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:border-gray-600"
         key={"Winnaar" + this.props.recipe.recipeId}
         style={imageStyle}
       >
