09808e942c73285d97547627a49e72cbfb0c8b86
IlyaQuateau <ilya.quateau@student.hogent.be>
2024-01-16 20:38:23 +0100

    file upload and conversion

--- cookieProject/src/components/UploadComponent.jsx
+++ cookieProject/src/components/UploadComponent.jsx
@@ -6,14 +6,6 @@ import { useState } from "react";
 import "../css/Upload.css";
 import * as Yup from "yup";
 import { useFormik } from "formik";
-/*const YourComponent = () => {
-  const [buttonText, setButtonText] = useState("Upload");
-
-  const handleClick = () => {
-    // Verander de tekst wanneer de knop wordt geklikt
-    setButtonText("Geslaagd!");
-  };
-}; */
 
 //formik en yup hier alsook toepassen
 
@@ -22,7 +14,8 @@ const uploadSchema = Yup.object().shape({
 });
 
 function UploadComponent() {
-  const [file, setFile] = useState();
+  const [files, setFiles] = useState([]);
+
   const { values, handleSubmit, handleChange, errors, dirty, isValid } =
     useFormik({
       initialValues: {
@@ -31,36 +24,81 @@ function UploadComponent() {
       onSubmit: (values) => {
         console.log(values);
         // post request API
+        handleUpload();
       },
       validationSchema: uploadSchema,
     });
-  function handleFile(event) {
-    setFile(event.target.files[0]);
-    console.log(event.target.files[0]);
-  }
 
-  function handleUpload() {
-    const formData = new FormData();
-    formData.append("file", file);
-    fetch("url", {
-      method: "POST",
-      body: formData,
-    })
-      .then((response) => response.json())
+  const handleFileChange = (event, position) => {
+    if (event.target.files) {
+      console.log("fileObj", event.target.files[0]);
+      let tempFIles = [...files];
+      tempFIles[position] = event.target.files[0];
+      setFiles(tempFIles);
+    }
+  };
+
+  const convertBase64 = (file) => {
+    return new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(file);
+
+      fileReader.onload = () => {
+        resolve(fileReader.result);
+      };
+
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+  };
+
+  const convertAllFiles = async () => {
+    let base64Files = [];
+    for (let i = 0; i < files.length; i++) {
+      const base64 = await convertBase64(files[i]);
+      base64Files.push(base64);
+    }
+    return base64Files;
+  };
+
+  const handleUpload = () => {
+    convertAllFiles()
       .then((result) => {
-        console.log("succes", result);
+        const convertedFiles = result;
+        console.log("convertedFiles:", convertedFiles);
+        // postFiles(convertedFiles); // post request naar API
       })
       .catch((error) => {
-        console.error("Error: ", error);
+        console.log(error);
       });
-  }
+
+    // const formData = new FormData();
+    // formData.append("files", files);
+    // fetch("url", {
+    //   method: "POST",
+    //   body: formData,
+    // })
+    //   .then((response) => response.json())
+    //   .then((result) => {
+    //     console.log("succes", result);
+    //   })
+    //   .catch((error) => {
+    //     console.error("Error: ", error);
+    //   });
+  };
 
   return (
     <div className="upload-container">
       <h2 className="text font-serif font-bold text-xl">
         Upload jouw gerecht!
       </h2>
-      <form>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          handleSubmit(event);
+        }}
+      >
         <div className="">
           <input
             className="upload-input"
@@ -68,7 +106,6 @@ function UploadComponent() {
             name="titel"
             value={values.titel}
             onChange={handleChange}
-            onSubmit={handleSubmit}
             placeholder="Voeg een titel toe"
             style={{ margin: "50px" }}
           />
@@ -79,7 +116,7 @@ function UploadComponent() {
             className="upload-input"
             type="file"
             name="file"
-            onChange={handleFile}
+            onChange={(event) => handleFileChange(event, 0)}
           />
         </div>
         <div>
@@ -88,7 +125,7 @@ function UploadComponent() {
             className="upload-input"
             type="file"
             name="file2"
-            onChange={handleFile}
+            onChange={(event) => handleFileChange(event, 1)}
             style={{ margin: "50px" }}
           />
         </div>
@@ -97,7 +134,7 @@ function UploadComponent() {
             className="upload-input"
             type="file"
             name="file3"
-            onChange={handleFile}
+            onChange={(event) => handleFileChange(event, 2)}
             style={{ margin: "10px" }}
           />
         </div>
@@ -106,6 +143,7 @@ function UploadComponent() {
           disabled={!isValid || !dirty}
           style={{ margin: "50px" }}
           className="border border-blue-300 rounded bg-blue-300 p-3  hover:bg-blue-600 focus:bg-blue-600"
+          onClick={handleUpload}
         >
           <p className="font font-medium">Upload</p>
         </button>
