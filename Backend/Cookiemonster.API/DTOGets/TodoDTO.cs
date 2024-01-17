504eb2c8af9278e99d78808266aba534c16017a4
bvreepin <brad.vanreepingen@student.hogent.be>
2024-01-17 17:21:02 +0100

    DTO's foreignkeys required gemaakt

--- Backend/Cookiemonster.API/DTOGets/TodoDTO.cs
+++ Backend/Cookiemonster.API/DTOGets/TodoDTO.cs
@@ -1,8 +1,12 @@
-﻿namespace Cookiemonster.API.DTOGets
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cookiemonster.API.DTOGets
 {
     public class TodoDTO
     {
+        [Required]
         public int UserId { get; set; }
+        [Required]
         public int RecipeId { get; set; }
     }
 }
--- Backend/Cookiemonster.API/DTOPosts/ImageDTOPost.cs
+++ Backend/Cookiemonster.API/DTOPosts/ImageDTOPost.cs
@@ -1,9 +1,11 @@
-﻿namespace Cookiemonster.API.DTOPosts
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cookiemonster.API.DTOPosts
 {
     public class ImageDTOPost
     {
         public string Base64Image { get; set; } = string.Empty;
-
+        [Required]
         public int RecipeId { get; set; }
     }
 }
